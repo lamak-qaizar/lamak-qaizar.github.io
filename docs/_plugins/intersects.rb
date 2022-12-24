3cfe186388cc0f149ca9502d4bfe1c63819ff472
Lamak <lamak@Lamak.local>
2022-12-24 21:44:12 +0530

    trying split inside intersects method

--- docs/_plugins/intersects.rb
+++ docs/_plugins/intersects.rb
@@ -1,7 +1,7 @@
 module Jekyll
   module ArrayIntersectionFilter
     def intersects(first, second)
-      return (first & second).size != 0
+      return (first.to_s.split(',').map(&:strip) & second.to_s.split(',').map(&:strip)).size != 0
     end
   end
 end
